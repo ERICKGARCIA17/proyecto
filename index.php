ad5d05276ccaed4566e5a157a09deaf5261b91da
ERICKGARCIA17 <114431989+ERICKGARCIA17@users.noreply.github.com>
2022-09-30 09:09:10 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CV</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet">
+
+
+</head>
+<body>
+
+	<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">CV Personal</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="navbar-nav mr-auto" id="navbarNav">
+				<ul class="navbar-nav" >
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#estudios">Estudios</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#portafolio">Portafolio</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#intereses">Intereses</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#contacto">Contacto</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<br>
+
+	<div class="container" style="padding-top: 50px">
+		<div class="row align-items-start">
+			<div class="col">
+				<h1 align="center" class="card-subtitle mb-2 text-muted" style="background-color: #280303; color:white;">Erick Andrade Garcia</h1><br><br>
+				<p align="center">“La cultura se adquiere leyendo libros; pero el conocimiento del mundo, que es mucho más necesario, sólo se alcanza leyendo a los hombres y estudiando las diversas ediciones que de ellos existen”.</p>
+			</div>
+
+
+			<div class="col" >
+				<img src="imagenes/im.jpeg" width=250 class="img-fluid rounded-start" alt="...">
+			</div>
+		</div>
+		<br>
+
+		<div id="estudios" class="container-fluid" style="background-color:#E6FDFF; color:white; padding-top:60px">
+			<div class="row text-center">
+				<h1 class="display-1" style="color:black;">ESTUDIOS</h1>
+
+
+				<div class="container">
+					<div class="row align-items-start">
+						<div class="col">
+
+
+							<table class="table">
+								<thead>
+									<tr>
+										<th scope="col" style="background-color: #D50A0A;" ></th>
+										<th scope="col" style="background-color: #D50A0A; color: white;">Institución educativa</th>
+
+									</tr>
+								</thead>
+								<tbody>
+									<tr align="left">
+										<th scope="row">1.-</th>
+										<td>Escuela Secundaria General Rafael Minor Franco</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">2.-</th>
+										<td>Colegio de Bachilleres Plantel 11 Panotla</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">3.-</th>
+										<td colspan="2">Universidad Tecnologoica de Huejotzingo</td>
+									</tr>
+								</tbody>
+
+								<thead>
+									<tr>
+										<th scope="col" style="background-color: #D50A0A";></th>
+										<th scope="col" style="background-color: #D50A0A; color: white;">Cursos</th>
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr align="left">
+										<th scope="row">1.-</th>
+										<td>NDG Linux</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">2.-</th>
+										<td>CSS Básico + Introduccion a Flexbox</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">3.-</th>
+										<td colspan="2">Curso de HTML5 y CSS3</td>
+									</tr>
+
+
+									<thead>
+										<tr>
+											<th scope="col" style="background-color: #D50A0A;"></th>
+											<th scope="col" style="background-color: #D50A0A; color: white;">Conocimientos y Habilidades</th>
+										</tr>
+									</thead>
+
+
+									<tbody>
+										<tr align="left">
+											<th scope="row"></th>
+											<td>
+												<ul>
+													<li type="square"> Diseño</li>
+													<li type="square"> Word</li>
+													<li type="square"> Power Point</li>
+													<br>
+													<li> Musico</li>
+													<li> Expresion Oral</li>
+												</ul>
+											</td>
+										</tr>
+									</tbody>
+
+
+
+
+								</table>
+
+							</div>
+
+							<div class="col">
+								<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" >
+									<div class="carousel-indicators">
+										<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+										<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+										<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+									</div>
+									<div class="carousel-inner">
+										<div class="carousel-item active">
+											<img src="imagenes/1.png" class="d-block w-100" alt="...">
+											<div class="carousel-caption d-none d-md-block">
+											</div>
+										</div>
+										<div class="carousel-item">
+											<img src="imagenes/2.jpg" class="d-block w-100" alt="...">
+											<div class="carousel-caption d-none d-md-block">
+											</div>
+										</div>
+										<div class="carousel-item">
+											<img src="imagenes/4.jpg" class="d-block w-100" alt="...">
+											<div class="carousel-caption d-none d-md-block">
+											</div>
+										</div>
+									</div>
+									<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+										<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+										<span class="visually-hidden">Previous</span>
+									</button>
+									<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+										<span class="carousel-control-next-icon" aria-hidden="true"></span>
+										<span class="visually-hidden">Next</span>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<br>
+
+		<div id="portafolio" class="container" style="padding-top: 50px">
+			<div class="row text-center">
+				<h1 class="display-1" style="background-color: #80F1F9">PORTAFOLIO</h1>
+				<div class="row align-items-start">
+
+					<div class="col">
+						<div class="card mb-3" style="max-width: 540px;">
+							<div class="row g-0">
+								<div class="col-md-4">
+									<img src="imagenes/primer.webp" class="img-fluid rounded-start" alt="...">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Primer cuatrimestre</h5>
+										<p class="card-text">El primer cuatrimestre se me hizo muy complicado por ser mi primer año escolar en la universidad, pero al momento de hacer nuestras tareas integradoras me dejo mucha experiencia y muy buenas amistades.</p>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card mb-3" style="max-width: 540px;">
+							<div class="row g-0">
+								<div class="col-md-4">
+									<img src="imagenes/segundo.jpg" class="img-fluid rounded-start" alt="...">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h5 class="card-title">Segundo cuatrimestre</h5>
+										<p class="card-text">Segundo Cuatrimestre de Universidad es un poco mas complicado de lo que pensaba pero a pesar de ello a un mantengo a mis grandes amistades y los tqm.</p>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<br>
+					<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img src="imagenes/xd.jpeg" class="d-block w-100" alt="..." width="640" height="550">
+							</div>
+							<div class="carousel-item">
+								<img src="imagenes/xd2.jpeg" class="d-block w-100" alt="..." width="640" height="550">
+							</div>
+							<div class="carousel-item">
+								<img src="imagenes/xd3.jpeg" class="d-block w-100" alt="..." width="640" height="550">
+							</div>
+						</div>
+						<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Previous</span>
+						</button>
+						<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Next</span>
+						</button>
+					</div>
+				</div>
+			</div>
+
+			<br>
+
+			<div id="intereses" class="container-fluid" style="background-color:#F0FCFF; color:black; padding-top: 50px">
+				<div class="row text-center">
+					<h1 class="display-1" style="color:black;">Intereses</h1>
+
+					<div class="row row-cols-1 row-cols-md-3 g-4">
+						<div class="col">
+							<div class="card">
+								<h1  class="card-title"><span class="material-icons">local_airport</span> </h1>
+								<div class="card-body">
+									<h5 class="card-title">Viajar</h5>
+									<p class="card-text">Siempre me ha interesado viajar por varias partes del mundo como por ejemplo me gustaria conocer cuba, de igual manera en un momento soñe con ser piloto.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col">
+							<div class="card">
+								<h1 class="card-title"><span class="material-icons">sports_soccer</span></h1>
+								<div class="card-body">
+									<h5 class="card-title">Deportes</h5>
+									<p class="card-text">Mi desporte que siempre me ha interesado y llamado la atencion a sido el futbol, desde chico lo he practicado</p>
+								</div>
+							</div>
+						</div>
+						<div class="col">
+							<div class="card">
+								<h1 class="card-title"><span class="material-icons">movie</span></h1>
+								<div class="card-body">
+									<h5 class="card-title">Cine</h5>
+									<p class="card-text">Me interesa mucho las peliculas de accion y de ciencia ficcion</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+
+		<div id="contacto" class="container-fluid" style="background-color:blueviolet; color:white;">
+			<div class="row text-center">
+				<h1 class="display-1">Contacto</h1>
+
+			</div>
+			<br>
+			<div class="row">
+				<div class="col-md-6 text-center">
+
+					<h3>REDES SOCIALES</h3>
+					<H1>
+						<a href="https://es-la.facebook.com/"><ion-icon name="logo-facebook"></ion-icon></a> 
+						<a href="https://twitter.com/?lang=es"><ion-icon name="logo-instagram"></ion-icon></a>
+						<a href="https://instagram.com/"><ion-icon name="logo-twitter"></ion-icon></a><br>	
+
+						<br>
+
+					</div>
+					<div class="col-md-6 text-center">
+						
+						<span class="material-icons">location_on </span> Panotla Tlaxcala <br>
+						<span class="material-icons">phone </span> 2462083681 <br>
+						<span class="material-icons">mail </span> 3521110183@uth.edu.mx 
+						
+						
+					</div>
+				</H1>
+			</div>
+		</div>
+		<br>
+		<p align="center">Derechos Reservados 2022</p>
+
+
+
+
+
+
+
+		<script src="js/bootstrap.bundle.min.js"></script>
+		<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+		<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+	</body>
+	</html>
